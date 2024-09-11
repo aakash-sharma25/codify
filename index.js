2f3ac6c6d0bb9996ae3d69d2f39b750b12ae0886
aakash-sharma25 <as024harma@gmail.com>
2024-09-11 16:24:34 +0530

    initail setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const { connectDB } = require("./config/db");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
