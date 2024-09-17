79af97623ccdf5e14e154091df8d9722032101c5
aakash-sharma25 <as024harma@gmail.com>
2024-09-17 14:27:20 +0530

    cofig changed

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
-const { connectDB } = require("./config/db");
+
 require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/admin.route");
+const { connectDB } = require("./config/db");
 
 const PORT = process.env.PORT || 8000;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app=express()
+
+app.use(cors({
+    origin:"*",
+    credentials:true
+}))
+
+app.use(express.json({limit:"20kb"})) 
+app.use(express.urlencoded({extended:true,limit:"20kb"}))
+app.use(cookieParser())
 
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port ${PORT}`);
 });
