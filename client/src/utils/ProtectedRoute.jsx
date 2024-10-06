5decfae486e659dfe68025d9b5ad2463c593ef93
aakash-sharma25 <as024harma@gmail.com>
2024-10-06 14:35:20 +0530

    added protected route boilerplate

--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -3,7 +3,8 @@ import { Outlet } from "react-router-dom";
 
 function ProtectedRoute({ role }) {
   console.log(role);
-  return <>{role === "Admin" ? <Outlet /> : null}</>;
+  // return <>{role === "Admin" ? <Outlet /> : null}</>;
+  return <Outlet/>
 }
 
 export default ProtectedRoute;
