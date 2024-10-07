90fd1c1546daaf7116d92f52e27eb559ad804d21
aakash-sharma25 <as024harma@gmail.com>
2024-10-07 21:09:34 +0530

    done some changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import { AppBar, Avatar, Box, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
-  const [anchorElUser, setAnchorElUser] =useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
   const navigate = useNavigate();
 
   const handleOpenUserMenu = (event) => {
@@ -12,6 +21,18 @@ export default function Navbar() {
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await axios.get("/api/v1/auth/logout", {
+        withCredentials: true,
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      alert("error in loggout ");
+    }
   };
   return (
     <>
@@ -30,41 +51,42 @@ export default function Navbar() {
               Lead Management
             </Typography>
             <Box display="flex" alignItems="center">
-              <Avatar onClick={handleOpenUserMenu} src="https://via.placeholder.com/150" alt="User Avatar" />
+              <Avatar
+                onClick={handleOpenUserMenu}
+                src="https://via.placeholder.com/150"
+                alt="User Avatar"
+              />
               <Box ml={2}>
                 <Typography variant="body1">Aakash Sharma</Typography>
                 <Typography variant="caption">Admin</Typography>
               </Box>
 
-
               <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={()=>navigate("/profile")}>
-                  <Typography sx={{ textAlign: 'center' }}>Profile</Typography>
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={() => navigate("/profile")}>
+                  <Typography sx={{ textAlign: "center" }}>Profile</Typography>
                 </MenuItem>
-              <MenuItem onClick={()=>navigate("/premium")}>
-                  <Typography sx={{ textAlign: 'center' }}>Premium</Typography>
+                <MenuItem onClick={() => navigate("/premium")}>
+                  <Typography sx={{ textAlign: "center" }}>Premium</Typography>
                 </MenuItem>
-              <MenuItem onClick={()=>navigate("/profile")}>
-                  <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
+                <MenuItem onClick={() => handleLogout()}>
+                  <Typography sx={{ textAlign: "center" }}>Logout</Typography>
                 </MenuItem>
-            </Menu>
-
-
+              </Menu>
             </Box>
           </Box>
         </Toolbar>
