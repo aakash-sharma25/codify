1308e5b863ff9c161356e95e9475aa51e8f03bfe
aakash-sharma25 <as024harma@gmail.com>
2024-10-13 11:12:14 +0530

    added some pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AllVendors from "./components/Admin/AllVendors";
 import Premium from "./components/Premium";
 import Profile from "./components/Profile";
+import ManagerLeads from "./components/Manager/ManagerLeads";
+import ManagerDashboard from "./components/Manager/ManagerDashboard";
+import ManagersEmployee from "./components/Manager/ManagersEmployee";
 
 const Login = React.lazy(() => import("./components/Login"));
 const Register = React.lazy(() => import("./components/Register"));
@@ -112,8 +115,39 @@ function App() {
           />
         </Route>
 
-
-        
+        <Route
+          path="/manager"
+          element={
+            <Suspense fallback={"Loading"}>
+              <ProtectedRoute role={"Manager"} />
+            </Suspense>
+          }
+        >
+          <Route
+            path="dashboard"
+            element={
+              <Suspense fallback={"Loading"}>
+                <ManagerDashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path="leads"
+            element={
+              <Suspense fallback={"Loading"}>
+                <ManagerLeads />
+              </Suspense>
+            }
+          />
+          <Route
+            path="employees"
+            element={
+              <Suspense fallback={"Loading"}>
+                <ManagersEmployee />
+              </Suspense>
+            }
+          />
+        </Route>
       </Routes>
     </Router>
   );
