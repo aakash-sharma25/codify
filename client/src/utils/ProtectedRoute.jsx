6f3c765f25d52e87b114b7a71de91da69889379b
aakash-sharma25 <as024harma@gmail.com>
2024-10-14 19:35:59 +0530

    added protectedRoute layout

--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 function ProtectedRoute({ role }) {
-  console.log(role);
+  // console.log(role);
   // return <>{role === "Admin" ? <Outlet /> : null}</>;
   return <Outlet/>
 }
