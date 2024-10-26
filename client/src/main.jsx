b8fcec51c948836588f03b80a26474bb5779c8c3
aakash-sharma25 <as024harma@gmail.com>
2024-10-26 16:25:14 +0530

    minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Toaster />
+  </StrictMode>
+);
