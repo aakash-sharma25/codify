79ce768e5b2a2f90314ecfad0c63ffd0efd86a40
aakash-sharma25 <as024harma@gmail.com>
2025-02-16 12:33:10 +0530

    added command

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const path = require("path")
+
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
